b0da469c7f58e7e66546d46ac422aad90408b58b
Ronak1605 <rpat256@aucklanduni.ac.nz>
2024-08-13 00:13:15 +1200

    Established initial DB connection
    Edited server.js such that the server connects to the DB when started.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
@@ -23,4 +24,11 @@ app.post("/api/reset", (req, res) => {
     res.json({ ing: ingredients });
 });
 
-app.listen(5000, () => console.log('Server is running on port 5000.'));
+// Connecting to the database
+require("dotenv").config({path: "./config.env"});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(5000, () => console.log('Connected to db and server is running on port 5000.'));
+    })
+    .catch((error) => console.error('Could not connect to MongoDB...'));
+
