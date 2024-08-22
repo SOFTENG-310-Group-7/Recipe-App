1cc38c506522d0743b0e8e9fc36cd40761361fcd
hayley9080 <hsmi309@aucklanduni.ac.nz>
2024-08-22 14:37:10 +1200

    refactor: made link fix sonar issue with assigning event listeners

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -2,16 +2,15 @@
 
 import Link from 'next/link';
 import { Button } from "@nextui-org/react";
-import { useRouter } from 'next/navigation';
 
 export default function Header() {
-  const router = useRouter(); 
-
   return (
     <div className="bg-white py-4 px-6 flex justify-between items-center">
-      <h1 className="text-4xl font-bold text-purple-700 cursor-pointer" onClick={() => router.push('/')}>
-        MEALMATCH
-      </h1>
+      <Link href="/" passHref>
+        <a className="text-4xl font-bold text-purple-700 cursor-pointer">
+          MEALMATCH
+        </a>
+      </Link>
       <div className="flex space-x-4">
         <Button color="primary" variant="ghost" size="lg">
           Saved Recipe
